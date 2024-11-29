f353e13298a7a3c241761032c0c44c0ae4973399
Recurring Oddity <danashby2000@gmail.com>
2024-11-28 22:14:27 -0500

    1.00.04

--- Game/Assets/Scripts/Pathfinding/Node.cs
+++ Game/Assets/Scripts/Pathfinding/Node.cs
@@ -5,8 +5,18 @@ public class Node
     public bool walkable;
     public Vector3 worldPosition;
 
+    //In case I forger: 
+    public int gCost; // Distance from start
+    public int hCost; // Distance from target
+
     public Node(bool _walkable, Vector3 _worldPosition){
         walkable = _walkable;
         worldPosition = _worldPosition;
+    }
+
+    public int fCost{
+        get{
+            return hCost + gCost;
+        }
     }
 }
--- Game/Assets/Scripts/Pathfinding/Pathfinding.cs
+++ Game/Assets/Scripts/Pathfinding/Pathfinding.cs
@@ -14,5 +14,20 @@ public class Pathfinding : MonoBehaviour
         List<Node> openSet = new List<Node>();
         HashSet<Node> closedSet = new HashSet<Node>();
         openSet.Add(startNode);
+
+        while(openSet.Count > 0){
+            Node currentNode = openSet[0];
+            for(int i = 1; i < openSet.Count; ++i){
+                if (openSet[i].fCost > currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost){
+                    currentNode = openSet[i];
+                }
+            }
+            openSet.Remove(currentNode);
+            closedSet.Add(currentNode);
+
+            if(currentNode == targetNode){
+                return;
+            }
+        }
     }
 }
